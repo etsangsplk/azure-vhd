ff2fa024d088b3f113b24010fa555e9cca7173b4
Huamin Chen <hchen@redhat.com>
2016-04-30 20:42:19 -0400

    add vhd list
    Signed-off-by: Huamin Chen <hchen@redhat.com>

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	
 	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/Azure/azure-sdk-for-go/arm/compute"
 	"github.com/Azure/azure-sdk-for-go/arm/examples/helpers"
 )
 
@@ -17,6 +18,19 @@ func main() {
 		azure.PublicCloud.ServiceManagementEndpoint)
 	if err != nil {
 		fmt.Println("azure: failed to acquire service principal token")
+		return
 	}
-	fmt.Printf("token: %+v\n", token)
+	//fmt.Printf("token: %+v\n", token)
+	client := compute.NewVirtualMachinesClient(cred["subscriptionID"])
+    client.Authorizer = token
+	vm, err := client.Get(cred["resourceGroup"], cred["vm"], "")
+	if err != nil {
+		fmt.Printf("failed to get VM: %+v\n", err)
+		return
+	}
+	disks := vm.Properties.StorageProfile.DataDisks;
+	for _, disk := range *disks {
+		//FIXME: check nil before reference
+		fmt.Printf("lun %d name %s vhd %s size(GB): %d\n", *disk.Lun, *disk.Name, *disk.Vhd.URI, *disk.DiskSizeGB)
+	}
 }
